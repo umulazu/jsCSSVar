0764d88d7dc5bc3e1a7dc689b3ccd7b07f3b6dac
umulazu <umulazu@gmail.com>
2018-06-16 20:29:02 +0300

    2 commit final version

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function handleUpdate() {
     const suffix = this.dataset.sizing || "";
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     // console.log(this.value);
+    // console.log(this.name);
 }
 
 inputs.forEach( input => input.addEventListener("change", handleUpdate));
